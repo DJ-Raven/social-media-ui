ee809fed846d8e45eb6a65985c615844206991a2
raven <laingraven.java@gmail.com>
2023-09-16 13:31:39 +0700

    first commit

---
+++ src/main/java/raven/login/Login.java
@@ -0,0 +1,86 @@
+package raven.login;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JPanel {
+
+    public Login() {
+        init();
+    }
+
+    private void init() {
+        setLayout(new MigLayout("fill,insets 20", "[center]", "[center]"));
+        txtUsername = new JTextField();
+        txtPassword = new JPasswordField();
+        chRememberMe = new JCheckBox("Remember me");
+        cmdLogin = new JButton("Login");
+        JPanel panel = new JPanel(new MigLayout("wrap,fillx,insets 35 45 30 45", "fill,250:280"));
+        panel.putClientProperty(FlatClientProperties.STYLE, "" +
+                "arc:20;" +
+                "[light]background:darken(@background,3%);" +
+                "[dark]background:lighten(@background,3%)");
+
+        txtPassword.putClientProperty(FlatClientProperties.STYLE, "" +
+                "showRevealButton:true");
+        cmdLogin.putClientProperty(FlatClientProperties.STYLE,"" +
+                "[light]background:darken(@background,10%);" +
+                "[dark]background:lighten(@background,10%);" +
+                "borderWidth:0;" +
+                "focusWidth:0;" +
+                "innerFocusWidth:0");
+
+        txtUsername.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Enter your username or email");
+        txtPassword.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Enter your password");
+
+        JLabel lbTitle = new JLabel("Welcome back!");
+        JLabel description = new JLabel("Please sign in to access your account");
+        lbTitle.putClientProperty(FlatClientProperties.STYLE, "" +
+                "font:bold +10");
+        description.putClientProperty(FlatClientProperties.STYLE, "" +
+                "[light]foreground:lighten(@foreground,30%);" +
+                "[dark]foreground:darken(@foreground,30%)");
+
+        panel.add(lbTitle);
+        panel.add(description);
+        panel.add(new JLabel("Username"), "gapy 8");
+        panel.add(txtUsername);
+        panel.add(new JLabel("Password"), "gapy 8");
+        panel.add(txtPassword);
+        panel.add(chRememberMe, "grow 0");
+        panel.add(cmdLogin, "gapy 10");
+        panel.add(createSignupLabel(), "gapy 10");
+        add(panel);
+    }
+
+    private Component createSignupLabel() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        panel.putClientProperty(FlatClientProperties.STYLE, "" +
+                "background:null");
+        JButton cmdRegister = new JButton("<html><a href=\"#\">Sign up</a></html>");
+        cmdRegister.putClientProperty(FlatClientProperties.STYLE, "" +
+                "border:3,3,3,3");
+        cmdRegister.setContentAreaFilled(false);
+        cmdRegister.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        cmdRegister.addActionListener(e -> {
+            System.out.println("Go Signup form");
+            //  Next update
+        });
+        JLabel label = new JLabel("Don't have an account ?");
+        label.putClientProperty(FlatClientProperties.STYLE, "" +
+                "[light]foreground:lighten(@foreground,30%);" +
+                "[dark]foreground:darken(@foreground,30%)");
+        panel.add(label);
+        panel.add(cmdRegister);
+        return panel;
+    }
+
+
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JCheckBox chRememberMe;
+    private JButton cmdLogin;
+}
---
+++ src/main/java/raven/main/Application.java
@@ -0,0 +1,32 @@
+package raven.main;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import raven.login.Login;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JFrame {
+
+    public Application() {
+        init();
+    }
+
+    private void init() {
+        setTitle("FlatLaf Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(new Dimension(1200, 700));
+        setLocationRelativeTo(null);
+        setContentPane(new Login());
+    }
+
+    public static void main(String[] args) {
+        FlatRobotoFont.install();
+        FlatLaf.registerCustomDefaultsSource("raven.themes");
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        FlatMacDarkLaf.setup();
+        EventQueue.invokeLater(() -> new Application().setVisible(true));
+    }
+}
